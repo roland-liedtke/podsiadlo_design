5bbc8b8d6933bf15888cfb27897164ad0fd64975
s28961-pj <s28961@pjwstk.edu.pl>
2024-07-10 23:58:22 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Podsiadło Design`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
